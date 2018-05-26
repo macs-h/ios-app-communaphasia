98077fc4876869a473322d0f87d8facfdf2cd0d9
Winston Downes <winston_downes@hotmail.com>
2018-05-26 14:56:33 +1200

    Stash.

--- Aphasia_com/Aphasia_com/ScriptsFolder/Algorithm Scripts/Pic_to_text.swift
+++ Aphasia_com/Aphasia_com/ScriptsFolder/Algorithm Scripts/Pic_to_text.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+func imageToText(cellList:[SelectViewCell]){
+    var returnString:[String]
+    
+    if cellList[0].type == noun {
+        returnString[0] = "The"
+        returnString[1] = list[0].keyword
+    }
+    
+    if cellList[1].type == adj {
+        if cellList[0].num == 1 {
+            returnString[2] = "is"
+        }
+        if cellList[0].num == 2 {
+            returnString[2] = "are"
+        }
+        returnString[3] = cellList[1].keyword
+    }
+    if cellList[1].type == noun {
+        if cellList[0].num == 1 {
+            returnString[3] = "is"
+        }
+        if cellList[0].num == 2 {
+            returnString[3] = "are"
+        }
+        returnString[2] = cellList[1].keyword
+    }
+    if cellList[1].type == verb {
+        if cellList[0].num == 1 {
+            returnString[3] = "is"
+        }
+        if cellList[0].num == 2 {
+            returnString[3] = "are"
+        }
+        returnString[2] = cellList[1].keyword
+    }
+}
+

187fb04e81632f2dbbf898bfcd2a5f447ec762b1
Max Huang <github@maxhuang.nz>
2018-08-07 21:09:58 +1200

    Added WordsAPI functionality. Major bugs found.
    WordsAPI functionality added to Utility.swift file. Currently, it has to
    implement a 2 second sleep function due to async in order to retrieve
    the data from the web. Working on figuring out other ways to manage
    this.
    
    In terms of bugs found (pretty sure this was fixed last time I checked)
    but the current issue is that when words that are NOT in the database
    are entered into the text field (to be translated) these words cause the
    program to crash when attributedString (which is an optional) is being
    assigned to attributedText to be displayed on the UI screen.
    Last I remember, any words that didn't exist were highlighted in red and
    the app would keep the user on the same input string until the user
    typed in a valid input. This is a serious issue we should definitely
    look into.

--- Dev Projects/tmp/tmp/main.swift
+++ Dev Projects/tmp/tmp/main.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-public class MyDelegate: NSObject, URLSessionDataDelegate {
+private class MyDelegate: NSObject, URLSessionDataDelegate {
     
-    public func urlSession(_ session: URLSession,
+    fileprivate func urlSession(_ session: URLSession,
                            dataTask: URLSessionDataTask,
                            didReceive data: Data) {
         if let dataStr = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
@@ -34,25 +34,6 @@ func getSynonym(_ word: String) {
     let task = session.dataTask(with: request as URLRequest)
     task.resume()
 }
-
-//
-//    let string = "https://wordsapiv1.p.mashape.com/words/stack/synonyms"
-//    let url = NSURL(string: string)
-//    let request = NSMutableURLRequest(url: url! as URL)
-//    request.setValue("yTv8TIqHmimshZvfKLil4h6A2zT2p11GQe5jsnr4XhZtyt69bm", forHTTPHeaderField: "X-Mashape-Key")
-//    request.setValue("wordsapiv1.p.mashape.com", forHTTPHeaderField: "X-Mashape-Host")
-//    request.httpMethod = "GET"
-//    let session = URLSession.shared
-//
-//    let request1 = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
-//        if let resp = response as? HTTPURLResponse {
-//            // Do what you want to do with your response.
-//            print("response", response)
-//        }
-//        print("npothin")
-//    }
-//    request1.resume()
-//}
 
 
 print("> start")

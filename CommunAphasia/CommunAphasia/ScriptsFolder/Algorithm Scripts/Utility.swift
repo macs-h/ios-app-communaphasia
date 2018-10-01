48fb549fd230ebe4b1e33186cd4b002f6f2b1f1c
Winston Downes <dowwi431@student.otago.ac.nz>
2018-10-01 20:50:46 +1300

    fixed merge issues

--- CommunAphasia/CommunAphasia/ScriptsFolder/Algorithm Scripts/Utility.swift
+++ CommunAphasia/CommunAphasia/ScriptsFolder/Algorithm Scripts/Utility.swift
@@ -40,7 +40,7 @@ class Utility {
     let categories = ["emotions","animals","food","activity","travel","objects","other"]
     
     //for creating a 2D array of types
-    let typeDict:[String:Int] = ["noun":0, "adj":1, "verb":2, "pronoun":3, "adverb":4, "modal":4, "preposition":5]
+    let typeDict:[String:Int] = ["noun":0, "adj":1, "verb":2, "pronoun":3, "adverb":4, "modal":4, "prep":5, "number":6]
     
     // Fields for the database.
     let CELL_TABLE = Table("cellTable")
@@ -278,7 +278,7 @@ class Utility {
      */
     func getCellsByCategory(category: String) -> [[(word: String, type: String, image: UIImage, suggestions: [String], grNum: String,category: String,tense: String)]] {
 
-        var cells = [[(word: String, type: String, image: UIImage, suggestions: [String], grNum: String,category: String,tense: String)]](repeating: [], count: 6)
+        var cells = [[(word: String, type: String, image: UIImage, suggestions: [String], grNum: String,category: String,tense: String)]](repeating: [], count: typeDict.count)
         let query = CELL_TABLE.select(KEYWORD,TYPE,IMAGE_LINK,RELATIONSHIPS,GR_NUM,CATEGORY,TENSE).filter(CATEGORY.like(category))
         do {
             for cell in try database.prepare(query) {
